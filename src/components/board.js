7a0e821cad5581e47a00650ead31c0a1fa3da4d6
tanabee <tanabee1986@gmail.com>
2019-06-23 13:53:28 +0900

    Board の rendering をループで処理

--- src/components/board.js
+++ src/components/board.js
@@ -11,25 +11,24 @@ export default class Board extends React.Component {
     );
   }
 
+  renderBoard() {
+    const size = 3;
+    return Array(size).fill(null).map((_, row) => {
+      return (
+        <div key={row} className="board-row">
+          {
+            Array(size).fill(null).map((_, col) => {
+              return (<>{this.renderSquare(row * size + col)}</>);
+            })
+          }
+        </div>
+      );
+    })
+  }
+
   render() {
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div>{this.renderBoard()}</div>
     );
   }
 }
