9c5c4358ceec1a8e4a4fc4d4eec6f0a0f7724144
tanabee <tanabee1986@gmail.com>
2019-06-23 10:17:05 +0900

    state によりクリック後の値を制御

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    }
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
